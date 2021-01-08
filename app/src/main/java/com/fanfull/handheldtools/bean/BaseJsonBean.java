be792b6fe148eab06962a08d3c660ee9fc88e092
orsoul <orsoul@foxmail.com>
2021-01-08 09:49:18 +0800

    - gradle 任务

--- app/src/main/java/com/fanfull/handheldtools/bean/BaseJsonBean.java
+++ app/src/main/java/com/fanfull/handheldtools/bean/BaseJsonBean.java
@@ -1,8 +1,14 @@
 package com.fanfull.handheldtools.bean;
 
+import com.fanfull.libhard.finger.bean.FingerBean;
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 /** json实体类 基类. */
@@ -56,6 +62,10 @@ public class BaseJsonBean<T> {
   }
 
   public static void main(String[] args) {
+    show2();
+  }
+
+  private static void show1() {
     String jsonObj =
         "{\"code\":1,\"data\":{\"uid\":70080,\"cid\":\"0023200002\",\"name\":\"a01\",\"privileges\":[\"audit\",\"isolate\",\"operate\"]}}";
     String jsonArr =
@@ -67,6 +77,33 @@ public class BaseJsonBean<T> {
     BaseJsonBean<List<UserInfoBean>> jsonBeanList =
         BaseJsonBean.jsonString2List(jsonArr, UserInfoBean.class);
     System.out.println(jsonBeanList);
+  }
+
+  private static void show2() {
+    Gson gson = new GsonBuilder().setExclusionStrategies(new ExclusionStrategy() {
+      @Override public boolean shouldSkipField(FieldAttributes f) {
+        System.out.println("shouldSkipField:" + f.getName());
+        return !f.getName().equals("fingerIndex")
+            && !f.getName().equals("userId")
+            && !f.getName().equals("feature")
+            && !f.getName().equals("fingerName");
+      }
+
+      @Override public boolean shouldSkipClass(Class<?> clazz) {
+        //System.out.println("shouldSkipClass:" + clazz);
+        return false;
+      }
+    }).create();
+
+    FingerBean fingerBean = new FingerBean(7, "CCCC", "fingerId", 0);
+    fingerBean.setUserId("userId");
+    fingerBean.setFingerName("fingerName");
+    fingerBean.setUserName("setUserName");
+    List<FingerBean> list = new ArrayList<>();
+    list.add(fingerBean);
+    String s = gson.toJson(list);
+    System.out.printf("json:%s\n", s);
+    System.out.printf("json:%s\n", new Gson().toJson(list));
   }
 
   public static class ParameterizedTypeImpl implements ParameterizedType {
